d5bc9ddf00ba1db5235334f0af29da009407ff5f
nourbahloull <157482532+nourbahloull@users.noreply.github.com>
2025-08-20 17:01:50 +0100

    Update environment.ts

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCpa5KvOAeECgX7TWUtQgxUbcqCFpbeY2w",
-    authDomain: "glidg2-a2544.firebaseapp.com",
-    projectId: "glidg2-a2544",
-    storageBucket: "glidg2-a2544.firebasestorage.app",
-    messagingSenderId: "272703414731",
-    appId: "1:272703414731:web:992e2c9695ee66296b964e",
-    measurementId: "G-6B4Y6TDMSJ"
+    apiKey: "your-api-key",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
